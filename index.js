f5d15ebd37e3b39fb9d5486b91450aa3513a8766
Drliamamir <104776232+Drliamamir@users.noreply.github.com>
2022-05-02 12:38:53 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-//
-
 const ColorList = [ "black" , "cornflowerblue" , "tomato"]
 const Tr = document.getElementById("ChangeColorBtn");
 const ColorNamer = document.getElementById("ColorName");
 
 Tr.addEventListener("click", function () {
-    const manycolor = ColorList.length
-    const colornumber = Math.random() * manycolor;
-    const colorn2umber = Math.floor(colornumber)
-    const colorname = ColorList[colorn2umber]
-    ColorNamer.innerHTML = colorname;
-    console.log(colorname)
-    document.body.style.background = colorname;
+    const many_color = ColorList.length
+    const color_number = Math.random() * many_color;
+    const new_colornumber = Math.floor(color_number)
+    const color_name = ColorList[new_colornumber]
+    ColorNamer.innerHTML = color_name;
+    document.body.style.background = color_name;
 })
